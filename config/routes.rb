8db2387607a6187b313f03b5ea771ecedaf5f12f
Cesar <cesarmejia451@gmail.com>
2016-02-23 21:29:32 -0600

    secured api with email and api key headers

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
-  get '/employees' => 'employees#index'
-  post '/employees' => 'employees#create'
-  patch 'employees/:id' => 'employees#update'
-  get 'employees/:id' => 'employees#show'
-
-  delete 'employees/:id' => 'employees#destroy'
-
+  namespace :api do
+    namespace :v1 do
+      get '/employees' => 'employees#index'
+      post '/employees' => 'employees#create'
+      patch '/employees/:id' => 'employees#update'
+      get '/employees/:id' => 'employees#show'
+      delete '/employees/:id' => 'employees#destroy'
+    end
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160216001048) do
+ActiveRecord::Schema.define(version: 20160224022011) do
 
   create_table "addresses", force: :cascade do |t|
     t.string   "address_1",   limit: 255
@@ -30,6 +30,13 @@ ActiveRecord::Schema.define(version: 20160216001048) do
     t.string   "email",      limit: 255
     t.date     "birthdate"
     t.string   "snn",        limit: 255
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "email",      limit: 255
+    t.string   "api_key",    limit: 255
     t.datetime "created_at"
     t.datetime "updated_at"
   end
